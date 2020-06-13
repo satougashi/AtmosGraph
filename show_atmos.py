7fee68f6c95ecfa64d495617bbb1d7387bea5aac
yoc <satougashi.amatou@gmail.com>
2020-06-13 09:29:50 +0900

    add limit to show atmos

--- show_atmos.py
+++ show_atmos.py
@@ -8,6 +8,6 @@ dbname = 'atmos.db'
 with closing(sqlite3.connect(dbname)) as conn:
   c = conn.cursor()
 
-  select_sql = 'select * from atmospheres'
+  select_sql = 'select * from atmospheres order by created_at desc limit 50;'
   for row in c.execute(select_sql):
       print(row)
